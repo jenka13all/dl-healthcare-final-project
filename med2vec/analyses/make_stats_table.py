39d5a15e47bcaa3c3d98a77f05b44bdf4b436400
Jennifer Koenig <jenka13all@gmail.com>
2021-05-07 15:18:00 +0200

    Clean up makes_stats table code for med2vec

--- med2vec/analyses/make_stats_table.py
+++ med2vec/analyses/make_stats_table.py
@@ -2,69 +2,91 @@ import pandas as pd
 import yaml
 import json
 import pickle
+import os
 
 '''
 maps the "task number" in the results JSON to care condition name
 formats the results JSON into tabular format and saves as csv file
 '''
 
-benchmark_path = '../../benchmarks/'
-resource_path = '../resources/'
 
-with open(benchmark_path + 'resources/hcup_ccs_2015_definitions_benchmark.yaml', 'r') as f:
+def convert_json_results_to_dict(results_json, phenotype_labels):
+    with open(results_json) as json_file:
+        results = json.load(json_file)
+
+    results_dict = {}
+    for i in range(1, 26):
+        key = "ROC AUC of task {}".format(i)
+        results_dict[i] = results[key]
+
+    # get the NAME of "Task x" (the care condition) along with its results
+    final_results_dict = {}
+    for i in range(1, 26):
+        key = phenotype_labels[i]
+        final_results_dict[key] = results_dict[i]
+
+    return final_results_dict
+
+
+def create_empty_final_results_dict(phenotype_labels):
+    # get the NAME of "Task x" (the care condition) along with its results
+    final_results_dict = {}
+    for i in range(1, 26):
+        key = phenotype_labels[i]
+        final_results_dict[key] = 0
+
+    return final_results_dict
+
+
+def get_results_table(filename, final_results_dict, train, test, definitions):
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+
+    # build final prevalence and AUC-ROC table
+    metrics_df = pd.DataFrame(columns=['Phenotype', 'Type', 'Train', 'Test', 'AUC-ROC'])
+    i = 0
+    key = 1
+    for rx, metrics in final_results_dict.items():
+        prev_train = train_prevalence_dict[key]
+        prev_test = test_prevalence_dict[key]
+
+        auc_roc = round(metrics['value'], 3)
+        rx_type = definitions[rx]['type']
+
+        metrics_df.loc[i] = [rx, rx_type, prev_train, prev_test, auc_roc]
+        i += 1
+        key += 1
+
+    # save final dataframe to CSV
+    metrics_df.to_csv(filename, float_format='%.3f')
+
+    return metrics_df
+
+
+local_resource_path = '../resources/'
+common_resources_path = '../../common_resources/'
+
+with open(common_resources_path + 'hcup_ccs_2015_definitions_benchmark.yaml', 'r') as f:
     definitions = yaml.load(f, Loader=yaml.FullLoader)
 
-# map "task number" (e.g. "1", "2") to correct conditions
-labels_file = benchmark_path + 'data/root/phenotype_labels.csv'
-labels = pd.read_csv(labels_file)
-labels = list(labels.head(1))
-
-phenotype_labels = {}
-for i in range(1, len(labels)+1):
-    phenotype_labels[i] = labels[i-1]
-
-'''
-with open(resources_path + 'task_nr_to_phenotype_label.dict', 'wb') as f1:
-    pickle.dump(phenotype_labels, f1)
-'''
+phenotype_labels = pickle.load(open(common_resources_path + 'task_nr_to_phenotype_label.dict', 'rb'))
 
 # open pheno_results.json
 results_json = '../Med2Vec_model/train_test_split_3_digit_icd/pheno.json'
-
-with open(results_json) as json_file:
-    results = json.load(json_file)
-
-results_dict = {}
-for i in range(1, 26):
-    key = "ROC AUC of task {}".format(i)
-    results_dict[i] = results[key]
-
-# get the NAME of "Task x" (the care condition) along with its results
-final_results_dict = {}
-for i in range(1, 26):
-    key = phenotype_labels[i]
-    final_results_dict[key] = results_dict[i]
+final_results_dict = convert_json_results_to_dict(results_json, phenotype_labels)
 
 # get care-condition prevalence in train and test populations
-train_prevalence_dict = pickle.load(open(resource_path + 'care_condition_train_prevalence.dict', 'rb'))
-test_prevalence_dict = pickle.load(open(resource_path + 'care_condition_test_prevalence.dict', 'rb'))
+train_prevalence_dict = pickle.load(open(local_resource_path + 'care_condition_train_prevalence.dict', 'rb'))
+test_prevalence_dict = pickle.load(open(local_resource_path + 'care_condition_test_prevalence.dict', 'rb'))
 
-# build final dataframe of care condition, type, prevalence for each patient split, AUC-ROC
-metrics_df = pd.DataFrame(columns=['Phenotype', 'Type', 'Train', 'Test', 'AUC-ROC'])
-i = 0
-key = 1
-for rx, metrics in final_results_dict.items():
-    prev_train = train_prevalence_dict[key]
-    prev_test = test_prevalence_dict[key]
+results = get_results_table(
+    '../Med2Vec_data/evaluate/results_table.csv',
+    final_results_dict,
+    train_prevalence_dict,
+    test_prevalence_dict,
+    definitions
+)
 
-    auc_roc = round(metrics['value'], 3)
-    rx_type = definitions[rx]['type']
-
-    metrics_df.loc[i] = [rx, rx_type, prev_train, prev_test, auc_roc]
-    i += 1
-    key += 1
-
-# save final dataframe to CSV
-#metrics_df.to_csv(resource_path+'results_table.csv', float_format='%.3f')
+print(results)
 
 
